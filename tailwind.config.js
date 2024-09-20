1b6a9e1bf55a2fd9357554091f237d01e62868b6
mukul667 <aditya.031298@gmail.com>
2024-09-20 13:09:30 +0530

    icons fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".html,js}"],  theme: {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
     extend: {},
   },
   plugins: [],
